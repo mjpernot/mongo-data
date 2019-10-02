24354424c0c1a55d7d064e3c0b81d29ddf997fce
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-02 12:51:00 +0100

    Update test/unit/mongo_db_data/delete_docs.py

--- test/unit/mongo_db_data/delete_docs.py
+++ test/unit/mongo_db_data/delete_docs.py
@@ -110,9 +110,9 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        class RepSet(object):
+        class RepSetCfg(object):
 
-            """Class:  RepSet
+            """Class:  RepSetCfg
 
             Description:  Class which is a representation of a RepSet class.
 
@@ -143,7 +143,7 @@ class UnitTest(unittest.TestCase):
                 self.repset = "RepSetName"
                 self.repset_hosts = ["List of hosts"]
 
-        self.repset = RepSet()
+        self.repset = RepSetCfg()
         self.args_array = {"-b": "databasename", "-t": "tablename",
                            "-a": "authdatabase"}
         self.args_array2 = {"-b": "databasename", "-t": "tablename",
