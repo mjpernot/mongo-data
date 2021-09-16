3ab5341a47930708e167611edcfd0f5d60d56775
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-09-16 16:54:29 +0100

    Update mongo_db_data.truncate_coll v3.0.0 - Added auth_mech to mongo_class instance call

--- mongo_db_data.py
+++ mongo_db_data.py
@@ -363,7 +363,8 @@ def truncate_coll(repclu, args_array, **kwargs):
         repclu.name, repclu.user, repclu.japd, host=repclu.host,
         port=repclu.port, auth=repclu.auth, repset=repclu.repset,
         repset_hosts=repclu.repset_hosts, db=args_array.get("-b"),
-        coll=args_array.get("-t"), db_auth=args_array.get("-a", None))
+        coll=args_array.get("-t"), db_auth=args_array.get("-a", None),
+        auth_mech=repclu.auth_mech)
     coll.connect()
 
     # Require override option.
